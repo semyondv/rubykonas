fda83727356ab598efda94680982b7020cd0aeb4
semyondv <semen.9056@mail.ru>
2019-09-17 07:20:59 +0300

    17.09

--- about_hashes.rb
+++ about_hashes.rb
@@ -19,7 +19,6 @@ class AboutHashes < Neo::Koan
     assert_equal 'dos', hash[:two]
     assert_equal nil, hash[:doesnt_exist]
   end
-
   def test_accessing_hashes_with_fetch
     hash = { :one => "uno" }
     assert_equal 'uno', hash.fetch(:one)
@@ -96,11 +95,11 @@ class AboutHashes < Neo::Koan
     hash[:one] << "uno"
     hash[:two] << "dos"
 
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:three]
+    assert_equal ['uno', 'dos'], hash[:one]
+    assert_equal ['uno', 'dos'], hash[:two]
+    assert_equal ['uno', 'dos'], hash[:three]
 
-    assert_equal __, hash[:one].object_id == hash[:two].object_id
+    assert_equal true, hash[:one].object_id == hash[:two].object_id
   end
 
   def test_default_value_with_block
@@ -109,8 +108,8 @@ class AboutHashes < Neo::Koan
     hash[:one] << "uno"
     hash[:two] << "dos"
 
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:three]
+    assert_equal ['uno'], hash[:one]
+    assert_equal ['dos'], hash[:two]
+    assert_equal [], hash[:three]
   end
 end
